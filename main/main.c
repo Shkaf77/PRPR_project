8506f306350f7ef1d02095e381fb48efeaae0da8
Олександр Ніценко <niroshi.0@MacBook-Air-Oleksandr-2.local>
2025-10-20 19:16:43 +0200

    Adding 'v1' function; Adding 'txt' files

---
+++ main/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int v1(FILE **fileSudoku, FILE **filePlayers, FILE **fileSolutions,
+       const char *fnSudoku, const char *fnPlayers, const char *fnSolutions)
+{
+    if (*fileSudoku == NULL) *fileSudoku = fopen(fnSudoku, "r");
+    if (*filePlayers == NULL) *filePlayers = fopen(fnPlayers, "r");
+    if (*fileSolutions == NULL) *fileSolutions = fopen(fnSolutions, "r");
+
+    if (*fileSudoku == NULL || *filePlayers == NULL || *fileSolutions == NULL) {
+        printf("V1: Neotvorene txt subory.\n");
+        return 0;
+    }
+
+    rewind(*filePlayers);
+    rewind(*fileSolutions);
+
+    return 0;
+}
+
+
+int main() {
+    FILE *f1 = NULL, *f2 = NULL, *f3 = NULL;
+
+    char *fname1 = "./Sudoku.txt";
+    char *fname2 = "./RegisterHracov.txt";
+    char *fname3 = "./RegisterRieseni.txt";
+
+    v1(&f1, &f2, &f3, fname1, fname2, fname3);
+
+    if (f1 != NULL) fclose(f1);
+    if (f2 != NULL) fclose(f2);
+    if (f3 != NULL) fclose(f3);
+    
+    return 0;
+}
+
+
